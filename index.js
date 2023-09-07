67ba5fc53b362f3620b98bf36613c1414086b1fc
Nini1997 <141282707+Nini1997@users.noreply.github.com>
2023-09-06 23:47:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,75 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+// Define the newImage function
+function newImage(src, left, bottom) {
+  const imageElement = document.createElement('img');
+  imageElement.src = src;
+  imageElement.style.position = 'fixed';
+  imageElement.style.left = left;
+  imageElement.style.bottom = bottom;
+  document.body.append(imageElement);
+}
+
+// Create the green character using the newImage function
+newImage('https://github.com/Nini1997/JS-Web-Game-Part-1/blob/main/assets/green-character.gif?raw=true', '100px', '100px');
+
+// Create the pine tree using the newImage function
+newImage('https://github.com/Nini1997/JS-Web-Game-Part-1/blob/main/assets/pine-tree.png?raw=true', '450px', '200px');
+
+// Define the newImage function
+function newImage(src, left, bottom) {
+  const imageElement = document.createElement('img');
+  imageElement.src = src;
+  imageElement.style.position = 'fixed';
+  imageElement.style.left = left;
+  imageElement.style.bottom = bottom;
+  document.body.append(imageElement);
+}
+
+// Create the green character using the newImage function
+newImage('assets/green-character.gif', '100px', '100px');
+
+// Create the pine tree using the newImage function
+newImage('assets/pine-tree.png', '450px', '200px');
+
+// Add more images
+newImage('https://github.com/Nini1997/JS-Web-Game-Part-1/blob/main/assets/tree.png?raw=true', '200px', '300px');
+newImage('https://github.com/Nini1997/JS-Web-Game-Part-1/blob/main/assets/pillar.png?raw=true', '350px', '100px');
+newImage('https://github.com/Nini1997/JS-Web-Game-Part-1/blob/main/assets/crate.png?raw=true', '150px', '200px');
+newImage('https://github.com/Nini1997/JS-Web-Game-Part-1/blob/main/assets/well.png?raw=true', '500px', '425px');
+
+let sword = document.createElement('img')
+sword.src = 'https://github.com/Nini1997/JS-Web-Game-Part-1/blob/main/assets/sword.png?raw=true'
+sword.position = 'fixed'
+sword.left = '500px'
+sword.bottom = '405px'
+document.body.append(sword)
+
+sword.addEventListener('click', function(){
+    sword.remove()
+})
+
+
+// Define the newImage function
+function newImage(src, left, bottom) {
+  const imageElement = document.createElement('img');
+  imageElement.src = src;
+  imageElement.style.position = 'fixed';
+  imageElement.style.left = left;
+  imageElement.style.bottom = bottom;
+  document.body.append(imageElement);
+}
+
+// Create the green character using the newImage function
+newImage('assets/green-character.gif', '100px', '100px');
+
+// Create the pine tree using the newImage function
+newImage('assets/pine-tree.png', '450px', '200px');
+
+// Add more images
+newImage('assets/tree.png', '200px', '300px');
+newImage('assets/pillar.png', '350px', '100px');
+newImage('assets/crate.png', '150px', '200px');
+newImage('assets/well.png', '500px', '425px');
+
+// Add shield and staff
+newImage('https://github.com/Nini1997/JS-Web-Game-Part-1/blob/main/assets/shield.png?raw=true', '165px', '185px');
+newImage('https://github.com/Nini1997/JS-Web-Game-Part-1/blob/main/assets/staff.png?raw=true', '600px', '100px');
